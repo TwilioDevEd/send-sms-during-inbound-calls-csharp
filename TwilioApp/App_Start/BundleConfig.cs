a3050403920af513bee9472d7c5afce434c0f95b
Carlos Villavicencio <cvillavicencio@stackbuilders.com>
2020-11-30 17:48:07 -0500

    Code Exchange Upgrade (#2)
    * Code Exchange Upgrade
    
    * Update readme template
    
    * Update .mergify.yml
    
    * Refactoring client side libraries management in favor of libman
    
    Co-authored-by: Maylon Pedroso <maylonpedroso@gmail.com>

--- TwilioApp/App_Start/BundleConfig.cs
+++ TwilioApp/App_Start/BundleConfig.cs
@@ -9,21 +9,13 @@ namespace TwilioApp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/lib/dist/js/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/lib/dist/js/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/Scripts/lib/dist/css/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
